2be6854fbec099f96a037af25a2db7490b9cf265
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2023-05-09 16:57:59 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://nodeblogapp.onrender.com/api",
+    baseURL: process.env.BACKEND_URL,
     withCredentials: false,
    
 })
